b1877d4105a8c7b970341dc50f180e2e9891712f
AnthoniaEfe <anthoniaefe36@gmail.com>
2025-04-16 00:46:51 +0100

    implement responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -44,6 +44,8 @@ const gridItems = [
   {image:[mobileFisheye, desktopFisheye], title: "make it fisheye"}
 ]
 
+const socialIcons = [faSquareFacebook, faTwitter, faPinterest, faInstagram]
+
 function App() {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
   const [isOpen, setIsOpen] = useState(false);
@@ -59,13 +61,13 @@ function App() {
 
   return (
     <>
-    <main className='font-Alata m-0 p-0 text-body-size' >
-    <nav className='flex flex-row m-0 px-4 py-8 gap-4 justify-between items-center fixed bg-[rgba(0,0,0,0)] z-10 top-0 left-o w-full text-white' aria-label="Main navigation">
+    <main className='font-display text-black font-light m-0 p-0 text-body-size' >
+    <nav className='flex flex-row m-0 px-4 md:px-20 py-8 gap-4 justify-between items-center absolute bg-[rgba(0,0,0,0)] z-10 top-0 left-0 w-full text-white' aria-label="Main navigation">
        <img src={logo} alt="loop studios logo" className='h-8 z-10'/>
 
         <div className='flex flex-row md:flex-row-reverse m-0 gap-4'>
           {isMobile ? <div className='flex flex-column m-0 p-0 justify-center items-center' onClick={() => setIsOpen(!isOpen)}  aria-controls="mobile-menu">
-                {isOpen ? (<FontAwesomeIcon icon={faXmark} className='z-10'/>) : (<FontAwesomeIcon icon={faBars} className='z-10'/>)}
+                {isOpen ? (<FontAwesomeIcon icon={faXmark} className='z-10 text-2xl'/>) : (<FontAwesomeIcon icon={faBars} className='z-10 text-2xl'/>)}
                 </div> : (
                 <ul className="flex flex-row m-0 gap-4 justify-center items-center" role="menubar">
                 {navItems.map((item) => (
@@ -80,13 +82,14 @@ function App() {
         {isOpen && (
           <div className="fixed top-0 right-full translate-x-[100%] p-8 w-full h-full text-white bg-black">
           <ul className="list-none flex flex-col gap-8 items-start justify-center h-full">
-            {navItems.map((item) => (
+            {navItems.map((item, index) => (
               <li
-                key={item}
-             className="text-[1.5rem] font-extrabold cursor-pointer hover:text-orange transition-colors duration-300 ease-in-out"
+                key={index}
                 role="menuitem"
               >
-                {item}
+                <a href="#" className="text-2xl cursor-pointer hover:">
+                  {item}</a>
+              
               </li>
             ))}
           </ul> 
@@ -94,38 +97,40 @@ function App() {
         )}
       </nav>
 
-      <header className="w-full h-auto relative bg-no-repeat bg-cover px-4 md:px-6 py-32 m-0 bg-[url('./assets/mobile/image-hero.jpg')] 
+      <header className="w-full h-auto relative bg-no-repeat bg-cover px-8 md:px-20 py-32 m-0 bg-[url('./assets/mobile/image-hero.jpg')] 
        md:bg-[url('./assets/desktop/image-hero.jpg')]">
-        <h1 className='text-6xl md:text-9l uppercase border-2 border-White text-White p-6 font-light mx-0 my-auto md:ml-4 md:mr-auto'>immersive experiences that deliver</h1>
+        <h1 className='font-josefin-sans w-full md:w-[60%] text-5xl md:text-8xl uppercase border-2
+         border-White text-White p-8 font-light mx-0 my-6 md:ml-4 md:mr-auto'>immersive experiences that deliver</h1>
       </header>
       
-      <section className="w-full h-auto p-4 md:p-12 m-0 flex flex-col md:flex-row gap-4 justify-center align-center relative">
+      <section className="w-full h-auto p-8 md:p-20 m-0 flex flex-col md:flex-row gap-4 justify-center align-center relative">
         <div className="w-full h-auto" >
           <img src={`${isMobile ? interactiveImgMobile : interactiveImgDesktop}`} alt="interactive image"/>
         </div>
-        <div className='p-6 md:p-10 md:absolute bg-white w-full md:w-[50%] bottom-0 right-0 flex flex-col justify-center align-center gap-6 md:gap-0'>
-          <h2 className='uppercase text-4l md:text-7xl text-center'>the leader in interactive vr</h2>
-          <p className='text-center w-full'>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies
+        <div className='p-8 md:p-12 md:absolute bg-white w-full md:w-[50%] bottom-20 right-0 flex flex-col justify-center align-center gap-6 md:gap-0'>
+          <h2 className='font-josefin-sans uppercase text-5xl md:text-7xl text-center'>the leader in interactive vr</h2>
+          <p className='text-2xl text-center w-full'>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies
             around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.
           </p>
         </div>
       </section>
 
-      <section className='w-full h-auto p-4 md:p-12 m-0'>
-        <h2 className='uppercase text-center text-4xl md:text-6xl my-6'>our creations</h2>
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-4 md:gap-2">
+      <section className='w-full h-auto p-8 pb-24 md:p-20 m-0 relative'>
+        <h2 className='font-josefin-sans uppercase text-5xl md:text-7xl text-center md:text-left my-12 w-full md:w-fit'>our creations</h2>
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-8 my-10 md:gap-2">
           {gridItems.map((item, index)=>
-          (<div key={index} className='w-full relative'>
-            <h3 className='text-White absolute left-5 bottom-5 uppercase text-2xl text-wrap w-[40%]'>{item.title}</h3>
+          (<div key={index} className='w-full relative hover:bg-[rgba(255,255,255,0.7)] text-Black'>
+            <h3 className='font-josefin-sans text-White absolute left-5 bottom-5 uppercase text-3xl text-wrap w-[40%]'>{item.title}</h3>
             {isMobile ? <img src={item.image[0]} alt={item.title}/> : <img src={item.image[1]} alt={item.title} />}
             </div>))}
         </div>
+        <button className='absolute bottom-12 left-0 translate-x-4/5 md:top-32 md:bottom-auto md:left-[75%] font-xl tracking-wider uppercase py-3 px-10 border-2 border-black hover:bg-black hover:text-white'>see all</button>
       </section>
 
-      <footer className='bg-Black text-white p-12 md:px-16 md:py-8 flex md:flex-row flex-col justify-around items-center gap-8'>
+      <footer className='bg-Black text-white py-20 px-12 md:px-20 md:py-10 flex md:flex-row flex-col justify-evenly md:justify-between items-center gap-10'>
         <div className="flex flex-col gap-6">
           <img src={logo} alt="loopstudios logo"/>
-          <ul className="flex flex-col md:flex-row m-0 gap-4 justify-center items-center" role="footermenu">
+          <ul className="flex flex-col md:flex-row m-0 gap-6 justify-center items-center" role="footermenu">
                 {navItems.map((item) => (
                   <li key={item} role="menuitem">
                     {item}
@@ -134,13 +139,12 @@ function App() {
         </div>
 
         <div className="flex flex-col gap-6">
-          <div className='flex flex-row gap-6 justify-center align-items'>
-          <FontAwesomeIcon icon={faSquareFacebook} />
-          <FontAwesomeIcon icon={faTwitter} />
-          <FontAwesomeIcon icon={faPinterest} />
-          <FontAwesomeIcon icon={faInstagram} />
+          <div className='flex flex-row gap-6 justify-center align-items md:justify-end'>
+            {socialIcons.map((item, index)=>(
+              <FontAwesomeIcon icon={item} key={index} className='text-2xl'/>
+            ))}
           </div>
-          <p>	&copy; 2021 Loopstudios. All rights reserved.</p>
+          <p className='text-grey'>	&copy; 2021 Loopstudios. All rights reserved.</p>
         </div>
       </footer>
     </main>
